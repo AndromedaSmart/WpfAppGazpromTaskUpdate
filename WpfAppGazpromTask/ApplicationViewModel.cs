7b322d0af2794561089dfabf6df4a0d2eba3c80a
asoro <asoro@DESKTOP-D6OSB31>
2024-01-24 00:53:17 +0300

    Добавьте файлы проекта.

---
+++ WpfAppGazpromTask/ApplicationViewModel.cs
@@ -0,0 +1,48 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Collections.ObjectModel;
+using System;
+using OxyPlot;
+using OxyPlot.Series;
+
+namespace WpfAppGazpromTask
+{
+    public class ApplicationViewModel
+    {
+        private Graph selectedTypeOfGraph;
+
+        public PlotModel MyModel { get; private set; }
+
+        public ObservableCollection<Graph> TypesOfGraphs { get; set; }
+        public Graph SelectedTypeOfGraph
+        {
+            get { return selectedTypeOfGraph; }
+            set
+            {
+                selectedTypeOfGraph = value;
+                OnPropertyChanged("selectedTypeOfGraph");
+            }
+        }
+
+        public ApplicationViewModel()
+        {
+            TypesOfGraphs = new ObservableCollection<Graph>
+            {
+                new Graph { Type = "Linear function" },
+                new Graph { Type = "Sinusoid function" },
+                new Graph { Type = "hyperbola function" },
+                new Graph { Type = "Point" }
+            };
+
+            this.MyModel = new PlotModel { Title = "Sinusoid function" };
+            this.MyModel.Series.Add(new FunctionSeries(Math.Sin, 0, 10, 0.1, "sin(x)"));
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName] string prop = "")
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+        }
+    }
+}
---
+++ WpfAppGazpromTask/Graph.cs
@@ -0,0 +1,41 @@
+﻿using OxyPlot;
+using OxyPlot.Series;
+using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace WpfAppGazpromTask
+{
+    public class Graph
+    {
+        private string type;
+
+        // private PlotModel myModel;
+        /* public PlotModel MyModel
+        {
+            get { return MyModel; }
+            private set
+            {
+                this.MyModel = new PlotModel { Title = "Sinusoid function" };
+                this.MyModel.Series.Add(new FunctionSeries(Math.Sin, 0, 10, 0.1, "sin(x)"));
+            }
+        }*/
+
+        public string Type
+        {
+            get { return type; }
+            set
+            {
+                type = value;
+                OnPropertyChanged("Type");
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName] string prop = "")
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+        }
+    }
+}
